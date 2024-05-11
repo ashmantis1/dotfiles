300840d9b28a8306698a86c36121bc9328cd94be
asher <leeasher090@gmail.com>
2024-05-11 16:05:42 +1000

    Initial neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+
+vim.opt.number = true
+vim.opt.hlsearch = true 
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.wrap = true
+-- TAB settings 
+vim.opt.tabstop = 2 
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2 
+vim.opt.expandtab = true 
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+# Setup symlinks 
+
+ln -s $(pwd)/nvim/init.lua $HOME/.config/nvim/init.lua
